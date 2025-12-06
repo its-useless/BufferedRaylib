d0a85d2acc2ec47b1ed53b3342eac124ffc5aeb7
aceinetx <mouselin82@gmail.com>
2025-12-06 15:31:07 +0300

    namespace

--- xmake.lua
+++ xmake.lua
@@ -1,3 +1,5 @@
+set_project("BufferedRaylib")
+
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"}) 
 
@@ -6,30 +8,32 @@ set_warnings("all")
 
 add_requires("raylib")
 
-target("buffered-raylib")
-    set_kind("static")
+namespace("buffered-raylib", function ()
+	target("buffered-raylib")
+			set_kind("static")
 
-    add_files(
-        "src/BufferedRaylib.cpp"
-    )
+			add_files(
+					"src/BufferedRaylib.cpp"
+			)
 
-    add_includedirs(
-        "src", 
-        {public = true}
-    )
+			add_includedirs(
+					"src", 
+					{public = true}
+			)
 
-    add_packages(
-        "raylib", 
-        {public=true}
-    )
-target_end()
+			add_packages(
+					"raylib", 
+					{public=true}
+			)
+	target_end()
 
-target("example")
-    set_kind("binary")
+	target("example")
+			set_kind("binary")
 
-    add_files(
-        "examples/test.cpp"
-    )
+			add_files(
+					"examples/test.cpp"
+			)
 
-    add_deps("buffered-raylib")
-target_end()
+			add_deps("buffered-raylib")
+	target_end()
+end)
